4f327344faff864c1f20f6add4bccb1d03599e5e
anguzudouglas <douuglas.anguzu@gmail.com>
2026-05-25 12:59:33 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 
 from app.routes.health import router as health_router
 from app.routes.stream import router as stream_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(
     title="CineGlas Stream API"
@@ -15,4 +16,14 @@ app.include_router(
 app.include_router(
     stream_router,
     prefix="/api"
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "https://vm-6xmqn0bzgaas1u3u1v8w1g49.vusercontent.net"
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
