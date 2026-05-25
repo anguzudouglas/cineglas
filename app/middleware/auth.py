ea9e6abb2c70303b95566e7b0b624316762ced8c
anguzudouglas <douuglas.anguzu@gmail.com>
2026-05-25 13:18:25 +0300

    Update auth.py

--- app/middleware/auth.py
+++ app/middleware/auth.py
@@ -1,9 +1,16 @@
-from fastapi import Header, HTTPException
+from fastapi import Header
+from fastapi import HTTPException
+
 from app.config import API_KEY
 
-def verify_api_key(x_api_key: str = Header(...)):
+def verify_api_key(
+    x_api_key: str = Header(...)
+):
+
     if x_api_key != API_KEY:
         raise HTTPException(
             status_code=401,
             detail="Invalid API key"
         )
+
+    return True
