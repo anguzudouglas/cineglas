c2e77722082930aa79f8aa46ae29435ccb6f911b
anguzudouglas <douuglas.anguzu@gmail.com>
2026-05-25 13:19:54 +0300

    Update stream.py

--- app/routes/stream.py
+++ app/routes/stream.py
@@ -1,15 +1,30 @@
 from fastapi import APIRouter
 from fastapi import Depends
+from fastapi import Request
+
+from slowapi import Limiter
+from slowapi.util import get_remote_address
 
 from app.middleware.auth import verify_api_key
 from app.services.ytdlp_service import extract_stream
 
 router = APIRouter()
 
+limiter = Limiter(
+    key_func=get_remote_address
+)
+
+
 @router.get("/stream/{youtube_id}")
-def get_stream(
+@limiter.limit("20/minute")
+async def get_stream(
+    request: Request,
     youtube_id: str,
-    _: str = Depends(verify_api_key)
+    _: bool = Depends(
+        verify_api_key
+    )
 ):
 
-    return extract_stream(youtube_id)
+    return extract_stream(
+        youtube_id
+    )
