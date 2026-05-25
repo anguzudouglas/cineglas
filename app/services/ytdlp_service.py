bec33fee168508edafdfc48f026c1bdc1b6f1840
anguzudouglas <douuglas.anguzu@gmail.com>
2026-05-25 14:40:08 +0300

    Update ytdlp_service.py

--- app/services/ytdlp_service.py
+++ app/services/ytdlp_service.py
@@ -1,46 +1,29 @@
 import yt_dlp
+from app.services.cache import stream_cache
 
 def extract_stream(youtube_id: str):
+
+    # return cached result if available
+    if youtube_id in stream_cache:
+        return stream_cache[youtube_id]
 
     url = f"https://www.youtube.com/watch?v={youtube_id}"
 
     options = {
         "quiet": True,
-        "noplaylist": True
+        "noplaylist": True,
+        "format": "best[height<=1080]"
     }
 
     with yt_dlp.YoutubeDL(options) as ydl:
+        info = ydl.extract_info(url, download=False)
 
-        info = ydl.extract_info(
-            url,
-            download=False
-        )
+    result = {
+        "title": info.get("title"),
+        "thumbnail": info.get("thumbnail"),
+        "stream_url": info.get("url")
+    }
 
-        formats = info.get("formats", [])
+    stream_cache[youtube_id] = result
 
-        video_formats = []
-
-        for fmt in formats:
-
-            if (
-                fmt.get("ext") == "mp4"
-                and fmt.get("vcodec") != "none"
-                and fmt.get("url")
-            ):
-                video_formats.append(
-                    {
-                        "quality": fmt.get("height"),
-                        "url": fmt.get("url")
-                    }
-                )
-
-        video_formats.sort(
-            key=lambda x: x["quality"] or 0,
-            reverse=True
-        )
-
-        return {
-            "title": info.get("title"),
-            "thumbnail": info.get("thumbnail"),
-            "streams": video_formats[:5]
-        }
+    return result
