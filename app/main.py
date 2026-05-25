dedbc3f33cda5b8d345dd5596b7f7020118da475
anguzudouglas <douuglas.anguzu@gmail.com>
2026-05-25 13:20:37 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,11 +1,40 @@
 from fastapi import FastAPI
 
-from app.routes.health import router as health_router
-from app.routes.stream import router as stream_router
+from slowapi import Limiter
+from slowapi.util import get_remote_address
+from slowapi.errors import RateLimitExceeded
+from slowapi.middleware import SlowAPIMiddleware
+
 from fastapi.middleware.cors import CORSMiddleware
 
+from app.config import FRONTEND_URL
+
+from app.routes.health import router as health_router
+from app.routes.stream import router as stream_router
+
 app = FastAPI(
-    title="CineGlas Stream API"
+    title="CineGlas API",
+    version="1.0.0"
+)
+
+limiter = Limiter(
+    key_func=get_remote_address
+)
+
+app.state.limiter = limiter
+
+app.add_middleware(
+    SlowAPIMiddleware
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        FRONTEND_URL
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(
@@ -16,14 +45,4 @@ app.include_router(
 app.include_router(
     stream_router,
     prefix="/api"
-)
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=[
-        "https://vm-6xmqn0bzgaas1u3u1v8w1g49.vusercontent.net"
-    ],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
 )
